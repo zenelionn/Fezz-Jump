9ba0ab2933aad11604e152dd2049a7bcfa048fd0
Eli <148476551+zenelionn@users.noreply.github.com>
2025-07-09 22:32:20 +0100

    make fezz flip plus spawn fixed

--- Fezziwig Jump/Assets/Scripts/GameManager.cs
+++ Fezziwig Jump/Assets/Scripts/GameManager.cs
@@ -7,23 +7,56 @@ public class GameManager : MonoBehaviour
 
     public GameObject platformPrefab;
 
-    public int platformCount = 300;
+    //public int platformCount = 300;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float xRange = 5;
+    [SerializeField] private float yRangeMin = 0.5f;
+    [SerializeField] private float yRangeMax = 2f;
+
+    [SerializeField] private Transform player;
+    [SerializeField] private float spawnDistanceAhead = 10f;
+    [SerializeField] private float despawnDistanceBelow = 15f;
+
+    private float lastSpawnY = 0f;
+    private List<GameObject> activePlatforms = new List<GameObject>();
+
+
+    // Update is called once per frame
+    
+
+    private void Update()
     {
-        Vector3 spawnPosition = new Vector3();
+        DespawnPlatforms();
 
-        for (int i = 0; i < platformCount; i++) {
-            spawnPosition.y += Random.Range(0.5f, 2f);
-            spawnPosition.x = Random.Range(-5f, 5f);
-            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        if (player.position.y + spawnDistanceAhead > lastSpawnY)
+        {
+            SpawnPlatform();
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DespawnPlatforms()
     {
-        
+        for (int i = activePlatforms.Count - 1; i >= 0; i--)
+        {
+            GameObject platform = activePlatforms[i];
+            if (platform != null && platform.transform.position.y < player.position.y - despawnDistanceBelow)
+            {
+                Destroy(platform);
+                activePlatforms.RemoveAt(i);
+            }
+        }
+    }
+
+
+    private void SpawnPlatform()
+    {
+        float y = lastSpawnY + Random.Range(yRangeMin, yRangeMax);
+        float x = Random.Range(-xRange, xRange);
+        Vector3 spawnPosition = new Vector3(x, y, 0f);
+
+        GameObject platform = Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        activePlatforms.Add(platform);
+
+        lastSpawnY = y;
     }
 }
--- Fezziwig Jump/Assets/Scripts/PlayerController.cs
+++ Fezziwig Jump/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,11 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 10f;
     public Rigidbody2D rb;
 
+    [SerializeField] private SpriteRenderer fezziwig;
+
+
     private float moveX;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -19,6 +23,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         moveX = Input.GetAxis("Horizontal") * moveSpeed;
+
+        // check what button is being pressed
+        // if left button, run switch to left and ect
+        if (Input.GetKeyDown(KeyCode.LeftArrow)|| (Input.GetKeyDown(KeyCode.A)))
+        {
+            //Debug.Log("left key");
+            SwitchSpriteLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || (Input.GetKeyDown(KeyCode.D)))
+        {
+            //Debug.Log("right key");
+            SwitchSpriteRight();
+        }
     }
 
     private void FixedUpdate()
@@ -26,5 +43,15 @@ public class PlayerController : MonoBehaviour
         Vector2 velocity = rb.velocity;
         velocity.x = moveX;
         rb.velocity = velocity;
+    }
+
+    private void SwitchSpriteLeft()
+    {
+        fezziwig.flipX = true;
+    }
+
+    private void SwitchSpriteRight()
+    {
+        fezziwig.flipX = false;
     }
 }
