47965d104cb2949539a865c28453c04ef0c41fbc
Eli <148476551+zenelionn@users.noreply.github.com>
2025-07-10 11:01:16 +0100

    switched cloud sprites

--- Fezziwig Jump/Assets/Scripts/Platform.cs
+++ Fezziwig Jump/Assets/Scripts/Platform.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
     public float jumpForce = 20f;
+
+
+
+
+    private void Start()
+    {
+
+        
+
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -18,5 +29,7 @@ public class Platform : MonoBehaviour
                 rb.velocity = velocity;
             }
         }
+
+
     }
 }
